15b14cd7d727fb217b01d7f17a5dbe75e6d56372
ARAFAT AMAN ALIM <76907565+Arafat-alim@users.noreply.github.com>
2021-07-31 01:29:39 +0530

    updated my apps

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,22 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            relatives : ["arafat", "Aquib", "Asif", "Mehtab", "Ganguly"],
+        }
+    }
+    
     render() {
-
+        
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               {
+                   this.state.relatives.map((data, i) => {
+                    return <div key={i}>{data}</div>;
+                })}
             </div>
         )
     }
